dfd8cf3c88bc88af8f2a98ee9c5abc5e9497288c
Keichi Takahashi <keichi.t@me.com>
2016-04-27 13:01:16 +0900

    ポート番号を環境変数から読むようにした

--- app.js
+++ app.js
@@ -76,7 +76,8 @@ app.get('/ping', function(req, res) {
     res.send('pong');
 });
 
-app.listen(5000, function () {
-  console.log('Example app listening on port 5000!');
+var port = process.env.PORT || 5000;
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port + '!');
 });
 
