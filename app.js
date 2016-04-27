34199d7d0681e987c3e55034b45d6e908688654b
Keichi Takahashi <keichi.t@me.com>
2016-04-27 12:57:32 +0900

    Change server port to 5000

--- app.js
+++ app.js
@@ -76,7 +76,7 @@ app.get('/ping', function(req, res) {
     res.send('pong');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(5000, function () {
+  console.log('Example app listening on port 5000!');
 });
 
